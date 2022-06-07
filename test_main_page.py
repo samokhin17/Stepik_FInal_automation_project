77fcb4215652c46b438c73ec142c7cf2eb00d260
samokhin17 <88451055+samokhin17@users.noreply.github.com>
2022-06-07 20:55:30 +0300

    Оформил переход между страницами

--- test_main_page.py
+++ test_main_page.py
@@ -1,14 +1,19 @@
 from selenium.webdriver.common.by import By
 from .Pages.main_page import MainPage
+from .Pages.login_page import LoginPage
+
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-<<<<<<< HEAD
+    link = link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
     page.open()
-    # page.go_to_login_page()
-    page.should_be_login_link()
-=======
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+    # login_page.should_be_login_url()
+    # login_page.should_be_login_page()
+    # login_page.should_be_register_form()
+
     browser.get(link)
     go_to_login_page(browser)
 
@@ -16,6 +21,6 @@ def test_guest_can_go_to_login_page(browser):
 def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
->>>>>>> 7bbb6b37df8a938f5aac60b59971bce0a8bc3e98
+
 
 # start command 'pytest -v --tb=line --language=en test_main_page.py'
