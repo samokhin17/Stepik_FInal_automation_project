da76b220b7ab7c098ebce68aa5385154eee89331
samokhin17 <88451055+samokhin17@users.noreply.github.com>
2022-06-12 19:28:20 +0300

    Добавлена параметризация

--- test_product_page.py
+++ test_product_page.py
@@ -1,16 +1,20 @@
-from selenium.webdriver.common.by import By
 from .Pages.product_page import ProductPage
-import time
+import pytest
+from .Pages.product_page import ProductPage
+
+product_base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+urls = [f"{product_base_link}/?promo=offer{no}" for no in range(10)]
 
 
-def test_guest_can_add_product_to_basket(browser):
+@pytest.mark.parametrize('link', urls)
+def test_guest_can_add_product_to_basket(browser, link):
     # Ссылка для проверки правильности имени и цены, после добавления в корзину
     # link = "https://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/"
     # Ссылка для добавления в конзину и рассчета значения. Снять коммент у метода "page.solve_quiz_and_get_code()"
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207'
+    # link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
-    # page.solve_quiz_and_get_code()
+    page.solve_quiz_and_get_code()
     page.should_be_correct_name()
     page.should_be_correct_price()
